6e86d7fb0e0d6f9c529ee0e00c251327ba5ca976
developer-mahin <mdmahin1310@gmail.com>
2023-05-10 23:25:37 +0600

    added job pages content

---
+++ src/Pages/Jobpage/jobPageStyle.js
@@ -0,0 +1,15 @@
+import { Box, styled } from "@mui/material";
+
+export const SafetyBoxWrapper = styled(Box)(({ theme }) => ({
+    padding: "15px 20px",
+    border: "1px solid #ddd", 
+    borderRadius: "5px", 
+    boxShadow: "0px 0px 15px -8px black"
+})) 
+
+export const JobContentWrapper = styled(Box)(({ theme }) => ({
+    padding: "15px 20px",
+    border: "1px solid #ddd", 
+    borderRadius: "5px", 
+    boxShadow: "0px 0px 15px -8px black"
+})) 
--- src/Pages/MyNetwork/MyNetwork.js
+++ src/Pages/MyNetwork/MyNetwork.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
+import { FaRegUserCircle } from 'react-icons/fa';
 
 const MyNetwork = () => {
+
+    const fakeData = [
+        { name: "Connectors", number: "140" },
+        { name: "Contacts", number: "130" },
+        { name: "Following & Followers", number: "8" },
+        { name: "Groups", number: "8" },
+        { name: "Events", number: "15" },
+        { name: "Pages", number: "200" },
+        { name: "Newsletters", number: "300" },
+        { name: "HashTags", number: "50" },
+    ]
+
+
     return (
         <div>
             <div className='border shadow position-sticky top-0 bg-white z-index'>
                 <Navbar></Navbar>
             </div>
-            <div className='container mx-auto px-3'>
-                <h1>My Network</h1>
+            <div className='mt-4'>
+                <div className='container mx-auto px-3 row'>
+                    <div className='col-md-4 border bg-secondary bg-opacity-10 p-4 rounded border'>
+
+                        {
+                            fakeData.map((data, i) => <div key={i} className='d-flex align-items-center justify-content-between py-2'>
+                                <div className='d-flex align-items-center gap-2'>
+                                    <div>
+                                        <FaRegUserCircle className='fs-4 text-secondary' />
+                                    </div>
+                                    <div>
+                                        <p className='m-0 fw-medium fs-5 text-secondary'>{data.name}</p>
+                                    </div>
+                                </div>
+                                <p className='m-0 text-secondary fw-medium fs-5'>{data.number}</p>
+                            </div>)
+                        }
+
+                    </div>
+                    <div className='col-md-8'>
+                        <div className='d-flex align-items-center justify-content-between border py-2 px-4'>
+                        <p className='m-0 fw-semibold'>Invitations</p>
+                        <p className='m-0 fw-semibold'>See All</p>
+                        </div>
+                        <div className='mt-2'>
+                            <button className='w-100 border btn fw-medium'>See More</button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
