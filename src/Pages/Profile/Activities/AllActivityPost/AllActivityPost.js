a657da24a807166573164e35e88018261adff084
Mahin Khan <99030147+developer-mahin@users.noreply.github.com>
2023-12-31 23:57:59 +0600

    Update AllActivityPost.js

--- src/Pages/Profile/Activities/AllActivityPost/AllActivityPost.js
+++ src/Pages/Profile/Activities/AllActivityPost/AllActivityPost.js
@@ -13,7 +13,7 @@ const AllActivityPost = () => {
     const { data: posts = [], isLoading } = useQuery({
         queryKey: ["posts"],
         queryFn: async () => {
-            const res = await fetch(`https://banao-project-server.vercel.app/user-post?email=${user?.email}`, {
+            const res = await fetch(`https://huntyourjob.vercel.app/user-post?email=${user?.email}`, {
                 headers: {
                     authorization: `Bearer ${localStorage.getItem("access-token")}`,
                     "content-type": "application/json"
