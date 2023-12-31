d7baefc588c7efd4932d8168f38a77be71706b9b
Mahin Khan <99030147+developer-mahin@users.noreply.github.com>
2023-12-31 23:49:55 +0600

    Update ContactInfoModal.js

--- src/Components/ContactInfoModal/ContactInfoModal.js
+++ src/Components/ContactInfoModal/ContactInfoModal.js
@@ -24,7 +24,7 @@ const ContactInfoModal = ({ contactInfoDataModal, customStyles, closeModalForInf
             city
         }
 
-        fetch(`https://banao-project-server.vercel.app/edit-contact/${profile._id}`, {
+        fetch(`https://huntyourjob.vercel.app/edit-contact/${profile._id}`, {
             method: "PATCH",
             headers: {
                 authorization: `Bearer ${localStorage.getItem("access-token")}`,
